ca7dc39f7d39b13dcafa9d15099c77abd6ff9856
maffort <maffort@gmail.com>
2021-12-15 11:22:12 -0300

    Criei classes de exemplo para RF001 Manter Pessoa.

---
+++ sgab-core/src/sgab/model/dto/util/PessoaHelper.java
@@ -0,0 +1,16 @@
+package sgab.model.dto.util;
+
+import sgab.model.dto.Pessoa;
+
+/**
+ *
+ * @author maffort <maffort@gmail.com>
+ */
+public class PessoaHelper {
+    public static boolean validarPessoa(Pessoa pessoa) {
+        
+        
+        
+        return true;
+    }
+}
---
+++ sgab-core/src/sgab/model/service/GestaoPessoas.java
@@ -0,0 +1,20 @@
+package sgab.model.service;
+
+import sgab.model.dto.Pessoa;
+import sgab.model.dto.util.PessoaHelper;
+/**
+ *
+ * @author maffort <maffort@gmail.com>
+ */
+public class GestaoPessoas {
+
+    void cadastrarPessoa(Pessoa pessoa) {
+
+        if (!PessoaHelper.validarPessoa(pessoa))
+            throw new RuntimeException("Os dados da pessoa não são válidos");
+        
+        // chamar dao de pessoa para persistir dado
+            
+    }
+    
+}
