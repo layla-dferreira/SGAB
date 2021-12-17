f0c5580601d5bdb1d9665a0b75647e35c38eb2b2
LuisBrescia <luisfelipebrescia@gmail.com>
2021-12-17 16:10:50 -0300

    Interface para class Autor

---
+++ sgab-core/src/sgab/model/dto/AutorInterface.java
@@ -0,0 +1,16 @@
+package sgab.model.dto;
+
+/**
+ * GRUPO F
+ * @author luisf
+ */
+interface AutorInterface {
+    
+    String getNacionalidade();
+    void setNacionalidade(String n);
+    String getGenero();
+    void setGenero(String n);
+    String getNascimento();
+    void setNascimento(String n);
+    String biografia();
+}
