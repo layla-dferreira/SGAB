c7425072403bdb02bf226c3bc8e2a9c8c11e06e0
victorschneider <70334871+victorschneider@users.noreply.github.com>
2021-12-21 15:09:49 -0300

    Update GestaoBiblioteca.java

--- sgab-core/src/sgab/model/service/GestaoBiblioteca.java
+++ sgab-core/src/sgab/model/service/GestaoBiblioteca.java
@@ -1,13 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package sgab.model.service;
+
+import sgab.model.dto;
+import sgab.model.dao.GerenciarBibliotecaDAO;
 
 /**
  *
  * @author Victor Schneider
  */
-public class GestaoBiblioteca {
+public class GestaoBiblioteca  {
 
+  public void GestaoBiblioteca(Biblioteca biblioteca){
+    
+      this.biblioteca = biblioteca;
+  }
+  
+  //metodos interface
+  //metodos bibliotecario
+  @Override
+  public void adicionarBibliotecario(Pessoa bibliotecario){
+    
+    biblioteca.adicionaBibliotecario(bibliotecario); 
+  }
+  @Override
+  public void removerBibliotecario(String nome){
+    
+    biblioteca.removeBibliotecarioProNome(nome); 
+  }
+  @Override
+  public Pessoa pesquisarBibliotecario(String nome){
+  
+    biblioteca.pesquisaBibliotecarioPorNome(nome);
+  }
+  
+  //metodos atendente
+  
+  @Override
+  public void adicionarAtendente(Pessoa atendente){
+    
+    biblioteca.adicionaAtendente(atendente); 
+  }
+  @Override
+  public void removerAtendente(String nome){
+    
+    biblioteca.removeAtendenteProNome(nome); 
+  }
+  @Override
+  public Pessoa pesquisarAtendente(String nome){
+  
+    biblioteca.pesquisaAtendentePorNome(nome);
+  }
+  
 }
