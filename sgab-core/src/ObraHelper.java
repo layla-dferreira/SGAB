3e7175c2c4cad3457d90179debaed5140b746c38
thiagoiulli <tiullybs@gmail.com>
2022-01-02 14:39:22 -0300

    rascunho DAO

---
+++ sgab-core/src/ObraHelper.java
@@ -0,0 +1,21 @@
+
+import sgab.model.dto.Pessoa;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author iulli
+ */
+public class ObraHelper {
+    public static boolean validarPessoa(Pessoa pessoa) {
+        
+        
+        
+        return true;
+    }
+}
