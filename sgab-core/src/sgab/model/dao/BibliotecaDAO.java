42e7546a184c0d0acd980b70b28005996f4fd6e1
Gabriel Guimarães <gasa12337@gmail.com>
2022-01-10 17:22:36 -0300

    Adição dos Status

--- sgab-core/src/sgab/model/dao/BibliotecaDAO.java
+++ sgab-core/src/sgab/model/dao/BibliotecaDAO.java
@@ -11,19 +11,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import sgab.model.dto.Biblioteca;
+import sgab.model.dto.util.BibliotecaStatus;
 
 
 public class BibliotecaDAO implements GenericDAO<Biblioteca, Long> {
     private static Map<Long, Biblioteca> bibliotecas = new HashMap<>();
     
+    private static Long idSequence; //  gera identificador sequencial
+    static {
+        BibliotecaDAO.idSequence = 0L;
+    }
+    
+    public static Long getNextId() {
+        return BibliotecaDAO.idSequence++;
+    }  
+    
     @Override
     public void inserir(Biblioteca biblioteca){
+        biblioteca.setId(this.getNextId());
         this.bibliotecas.put(biblioteca.getId(), biblioteca);
     }
     
     
     @Override
     public void alterar(Biblioteca biblioteca){
+        biblioteca.setId(this.getNextId());
         this.bibliotecas.put(biblioteca.getId(), biblioteca);
     }
     
@@ -60,6 +72,18 @@ public class BibliotecaDAO implements GenericDAO<Biblioteca, Long> {
         }
     }
    
+    
+    public void alterarStatus(String nome, BibliotecaStatus status){
+        for(Long key : this.bibliotecas.keySet()){
+            
+            Biblioteca valor = this.bibliotecas.get(key);
+            if(valor.getNome().equals(nome)){
+                valor.setStatus(status);
+                break;
+            }
+        }
+    }
+    
     @Override
     public Biblioteca pesquisar(Long key){
         return (this.bibliotecas.get(key));
--- sgab-core/src/sgab/model/dto/Biblioteca.java
+++ sgab-core/src/sgab/model/dto/Biblioteca.java
@@ -5,25 +5,28 @@ package sgab.model.dto;
  */
 
 import java.util.*;
+import sgab.model.dto.util.BibliotecaStatus;
 
 public class Biblioteca{
 
     private String unidadeorg;
     private String nome;
     private Long id;
+    private BibliotecaStatus status;
 
     //construtor
     public void Biblioteca(String unidadeorg, String nome, Long id){
         this.unidadeorg = unidadeorg;
         this.nome = nome;
         this.id = id;
+        this.status = BibliotecaStatus.ATIVA;
     }
     
     //setters
     public void setUnidadeOrg(String unidadeorg){
-    
         this.unidadeorg = unidadeorg;
     }
+    
     public void setNome(String Nome){
     
         this.nome = nome;
@@ -31,6 +34,12 @@ public class Biblioteca{
     public void setId(Long id){
     
         this.id = id;
+    }
+
+    public void setStatus(BibliotecaStatus status) {
+        if(this.status != BibliotecaStatus.SUSPENSA){
+           this.status = status; 
+        }
     }
     
     //getters
@@ -42,5 +51,8 @@ public class Biblioteca{
     }
     public long getId(){
         return id;
+    }
+    public BibliotecaStatus getStatus() {
+        return status;
     }
 }
