c2633c4609397591e2899202c5106a1dcc3adf53
Luisa <63885146+StaryLu@users.noreply.github.com>
2022-01-11 01:53:08 -0300

    Delete sgab-core/src/sgab/model directory

--- sgab-core/src/sgab/model/dao/GenericDeleteDAO.java
+++
@@ -1,5 +0,0 @@
-package sgab.model.dao;
-
-public interface GenericDeleteDAO<E, K> extends GenericDAO<E, K>{
-    void delete(K key);
-}
--- sgab-core/src/sgab/model/dao/readme.txt
+++
@@ -1,1 +0,0 @@
-Pacote conterá apenas classes Data Access Object -> realizam persistência das classes DTO.
